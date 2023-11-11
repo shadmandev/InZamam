f1dd30233f216214d7ad7f29cfdda9ec0cba8cca
shadmandev <shahibshadman@gmail.com>
2023-11-11 22:30:56 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UserController::class, 'sayHello']);
 Route::get('/hello', [UserController::class, 'sayHello']);
